8415cfd9e81776366e161966e684eaae052d7ba8
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2025-07-03 12:16:56 +0300

    fixed rooms fetching, add No. of ppl in room, full rooms and rooms creation

--- src/app/roomCard.ts
+++ src/app/roomCard.ts
@@ -7,8 +7,14 @@ import { Router } from "@angular/router";
     
     <div class="p-4 m-4 bg-white rounded-2xl flex flex-col items-center"> room title : {{title()}}
 
+    <p> {{ppl()}}/2</p>
+    @if (ppl() <2){
 
-    <button (click)="goToRoom()" class="m-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 active:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer transition w-fit">Join room!</button>
+        <button (click)="goToRoom()" class="m-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 active:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer transition w-fit">Join room!</button>
+    }@else{
+        Sorry, this room is full!
+    }
+    
     </div>`
     ,
     selector:"RoomCard"
@@ -17,6 +23,7 @@ export class RoomCard{
 
 title = input("room Title")
 roomID = input("roomID")
+ppl = input(0)
 router = inject(Router)
 
 goToRoom(){
--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -1,6 +1,8 @@
 import { Component, inject, signal } from "@angular/core";
 import { RoomCard } from "./roomCard";
 import { HttpClient } from "@angular/common/http";
+import { CreateRoomWindow } from "./createRoomWindow";
+import { Router } from "@angular/router";
 
 @Component({
 
@@ -12,12 +14,13 @@ import { HttpClient } from "@angular/common/http";
 
   <h1>Rooms, welcome to Rum rum</h1>
 
-
+    <CreateRoomWindow [visible]="roomCreationWindowVisible" (close)="roomCreationWindowVisible = false" (create)="createARoom($event)"/>
+    <button class=" bg-red-400 p-2 m-4 rounded hover:bg-red-600 active:bg-red-800 text-red-50" (click)="roomCreationWindowVisible = true">Create a room</button>
     <div class=" w-full h-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" >
         
 
       @for (room of rooms(); track $index){
-        <RoomCard [title]="room.title" [roomID]="room.ID"/>
+        <RoomCard [title]="room.title" [roomID]="room.ID" [ppl]="room.ppl"/>
 
     } @empty {
 
@@ -31,7 +34,7 @@ import { HttpClient } from "@angular/common/http";
     `,
 
     
-        imports:[RoomCard],
+        imports:[RoomCard,CreateRoomWindow],
 
     
     
@@ -42,18 +45,39 @@ import { HttpClient } from "@angular/common/http";
 
 export class Rooms{
 
-
-    rooms = signal<{title:string, ID:string}[]>([])
+    roomCreationWindowVisible=false
+    rooms = signal<{title:string, ID:string, ppl:number}[]>([])
     httpClient = inject(HttpClient)
+    router = inject(Router)
+
+
+    createARoom(data : {
+        rt:string,
+        rk:number
+    }){
+
+    this.httpClient.post<{
+        id:string
+    }>("http://localhost:3000/nr",data).subscribe((data)=>{
+        this.roomCreationWindowVisible=false;
+        this.router.navigate([`/room/${data.id}`]);
+        
+        
+
+    });
+
+
+    }
     constructor(){
 
 
         //fetch data
         this.httpClient.post<unknown>("http://localhost:3000/gr",{}).subscribe((d)=>{
-
-            if(d){
-                console.log(d);
-                this.rooms.set((d as []).map((data)=>{return {title:data[0],ID:data[1]}}))
+            console.log(d);
+            
+            if(d && Array.isArray(d)){
+                
+                this.rooms.set((d as []).map((data)=>{return {title:data[0],ID:data[1],ppl:data[2]}}))
                 // this.rooms.set(d);
             }
 
