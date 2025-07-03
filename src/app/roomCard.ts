f4ee524303c6fd170d87df79eff06cbe77b91d8c
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2025-07-03 13:13:53 +0300

    made locked rooms actually locked

--- src/app/roomCard.ts
+++ src/app/roomCard.ts
@@ -10,7 +10,12 @@ import { Router } from "@angular/router";
     <p> {{ppl()}}/2</p>
     @if (ppl() <2){
 
-        <button (click)="goToRoom()" class="m-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 active:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer transition w-fit">Join room!</button>
+        @if (locked()) {
+            <div class="font-bold text-sm">You need to be invited in order to join this room :/</div>
+        }@else {
+                    <button (click)="goToRoom()" class="m-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 active:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-400 cursor-pointer transition w-fit">Join room!</button>
+
+        }
     }@else{
         Sorry, this room is full!
     }
@@ -25,6 +30,7 @@ title = input("room Title")
 roomID = input("roomID")
 ppl = input(0)
 router = inject(Router)
+locked= input(false)
 
 goToRoom(){
 this.router.navigate([`/room/${this.roomID()}`])
